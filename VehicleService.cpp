morimoto/renesas-android-device-renesas-kingfisher

Initial commit for Android Q
Jira-Id: ADM-3193

Signed-off-by: Oleh Cherpak <oleh.cherpak@globallogic.com>
Change-Id: Idcc32462af5a04f5f342d5cb568cf981f96c1255

hal/vehicle/VehicleService.cpp
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "VehicleHalImpl"
+
+#include <iostream>
+
+#include <android-base/logging.h>
+#include <android-base/macros.h>
+
+#include <hidl/HidlTransportSupport.h>
+#include <vhal_v2_0/VehicleHalManager.h>
+
+#include "VehicleHalImpl.h"
+
+using namespace android;
+using namespace android::hardware;
+using namespace android::hardware::automotive::vehicle::V2_0;
+
+int main(int /* argc */, char* /* argv */ []) {
+    auto store = std::make_unique<VehiclePropertyStore>();
+    auto hal = std::make_unique<kingfisher::VehicleHalImpl>(store.get());
+    auto service = std::make_unique<VehicleHalManager>(hal.get());
+
+    configureRpcThreadpool(4, true /* callerWillJoin */);
+
+    CHECK_EQ(service->registerAsService(), android::NO_ERROR)
+      << "Failed to register vehicle HAL";
+
+    joinRpcThreadpool();
+}

morimoto/renesas-android-vendor-renesas-hal-vehicle

Jira-Id: ADM-3277

VehicleService.cpp
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "VehicleHalImpl"
+
+#include <iostream>
+
+#include <android-base/logging.h>
+#include <android-base/macros.h>
+
+#include <hidl/HidlTransportSupport.h>
+#include <vhal_v2_0/VehicleHalManager.h>
+
+#include "VehicleHalImpl.h"
+
+using namespace android;
+using namespace android::hardware;
+using namespace android::hardware::automotive::vehicle::V2_0;
+
+int main(int /* argc */, char* /* argv */ []) {
+    auto store = std::make_unique<VehiclePropertyStore>();
+    auto hal = std::make_unique<renesas::VehicleHalImpl>(store.get());
+    auto service = std::make_unique<VehicleHalManager>(hal.get());
+
+    configureRpcThreadpool(4, true /* callerWillJoin */);
+
+    CHECK_EQ(service->registerAsService(), android::NO_ERROR)
+      << "Failed to register vehicle HAL";
+
+    joinRpcThreadpool();
+}