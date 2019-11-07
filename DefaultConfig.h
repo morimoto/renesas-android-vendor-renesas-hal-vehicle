fdeab111b4cac12683c9060745c9ac1e6cbad3a5
Sergii Piatakov <sergii.piatakov@globallogic.com>
2019-11-07 15:00:23 +0200

    change default gear selection from PARK on NEUTRAL
    The default selection is transmitted when the real state is not ready.
    For example, if GPIO driver is not loaded the default state will be
    transmitted. It may lead to undesired side-effects, for example: if
    PARK mode is transmitted then EVS application will be enabled.
    
    It is proposed to change the default state to eliminate any side-effects.
    
    Test: disable GPIO driver in kernel configs `CONFIG_KEYBOARD_GPIO=n`,
          build image and flush a DUT, boot and ensure that there are not any
          undesired side-effects.
    Jira-Id: ADM-3914
    Change-Id: I3e6188d2d156dd12e0d050d67dd00e79e30faeb1
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- DefaultConfig.h
+++ DefaultConfig.h
@@ -636,7 +636,7 @@ const ConfigDeclaration kVehicleProperties[]{
             .access = VehiclePropertyAccess::READ,
             .changeMode = VehiclePropertyChangeMode::ON_CHANGE,
         },
-        .initialValue = {.int32Values = {toInt(VehicleGear::GEAR_PARK)}}
+        .initialValue = {.int32Values = {toInt(VehicleGear::GEAR_NEUTRAL)}}
     },
     {
         .config =
